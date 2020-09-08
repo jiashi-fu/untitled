1a509e2af60bf904f655e06630540015464e582d
jiashi.fu <1980046815@qq.com>
2020-09-08 13:52:06 +0800

    第三次提交

--- src/main/java/hellotest.java
+++ src/main/java/hellotest.java
@@ -3,13 +3,13 @@ import javax.crypto.spec.PSource;
 public class hellotest {
     public static void main(String[] args) {
         for (int i=0;i<100;i++){
-            System.out.println(i);
+            System.out.println("i"+i);
         }
         for (int j=0;j<100;j++){
-            System.out.println(j);
+            System.out.println("j"+j);
         }
-        for(int k=0;k<100;k++){
-           System.out.println(k);
+        for (int h=0;h<100;h++){
+            System.out.println("h"+h);
         }
 }
 }
