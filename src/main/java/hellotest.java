0ea5d9cbf8da93aa0feda594c44a05903517bbc2
jiashi.fu <1980046815@qq.com>
2020-09-08 11:39:54 +0800

    第二次提交

--- src/main/java/hellotest.java
+++ src/main/java/hellotest.java
@@ -5,6 +5,8 @@ public class hellotest {
         for (int i=0;i<100;i++){
             System.out.println(i);
         }
-
+        for (int j=0;j<100;j++){
+            System.out.println(j);
+        }
 }
 }
