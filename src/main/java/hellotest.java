fdd6702995103f1f15e53c1dc92386aa70812fed
jiashi.fu <1980046815@qq.com>
2020-09-08 11:34:13 +0800

    第二次提交

--- src/main/java/hellotest.java
+++ src/main/java/hellotest.java
@@ -1,5 +1,10 @@
 import javax.crypto.spec.PSource;
 
 public class hellotest {
+    public static void main(String[] args) {
+        for (int i=0;i<100;i++){
+            System.out.println(i);
+        }
 
+}
 }
